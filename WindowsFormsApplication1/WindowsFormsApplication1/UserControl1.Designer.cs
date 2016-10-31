baefb9c0f69d1cf81e7a60ef53d66ae4f56474cd
k.ishimoto <k.ishimoto@potatosalad.xyz>
2016-10-31 19:18:36 +0900

    branch2 test

--- WindowsFormsApplication1/WindowsFormsApplication1/UserControl1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/UserControl1.Designer.cs
@@ -49,7 +49,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "初期ラベルbranch";
+            this.label1.Text = "初期ラベルbranch2";
             // 
             // UserControl1
             // 
