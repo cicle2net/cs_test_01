000acaa1cdda3a23936fd71e85075ebc72dd30ee
k.ishimoto <k.ishimoto@potatosalad.xyz>
2016-10-31 16:50:59 +0900

    change screen

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+
+        public static UserControl1 ctr1;
+        public static UserControl2 ctr2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ctr1 = new UserControl1();
+            ctr2 = new UserControl2();
+
+            panel1.Controls.Add(ctr1);
+            panel1.Controls.Add(ctr2);
+
+            ctr1.Visible = true;
+            ctr2.Visible = false;
+
+        }
+    }
+}
